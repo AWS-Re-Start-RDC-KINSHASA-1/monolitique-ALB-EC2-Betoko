b01e15f8bf59d3389350e9dc9c0ba07711cd1a96
chrisianbetoko <christianbetoko@gmail.com>
2024-02-16 15:31:17 +0100

    Update app.py

--- app.py
+++ app.py
@@ -3,9 +3,10 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def overload():
-  while True:
-    pass
+def hello_world():
+  return "Hello, World!"
 
 if __name__ == "__main__":
+  
+
   app.run(host="0.0.0.0", port=5000, debug=true)
